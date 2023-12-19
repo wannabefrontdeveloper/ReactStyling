b67a4809025d5865ea04884d7cb2e10a7e8a3868
wannabefrontdeveloper <giwonk786064@gmail.com>
2023-12-19 15:25:31 +0900

    styled-components 매뉴얼에서 제공하는 유틸 함수를 사용하여 작업을 함수화하였습니다.

--- src/StyledComponent.js
+++ src/StyledComponent.js
@@ -1,5 +1,21 @@
 import styled, { css } from "styled-components";
 
+const sizes = {
+  desktop: 1024,
+  tablet: 768,
+};
+
+//위에 있는 size 객체에 따라 자동으로 media 쿼리 함수를 만들어준다.
+//참고: https://www.styled-components.com/docs/advanced#media-templates
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)};
+    }
+  `;
+  return acc;
+}, {});
+
 const Box = styled.div`
   /* props로 넣어준 값을 직접 전달해 줄 수 있다. */
   background: ${(props) => props.color || "blue"};
@@ -10,12 +26,8 @@ const Box = styled.div`
    768px 미만이 되면 꽉 채웁니다. */
   width: 1024px;
   margin: 0 auto;
-  @media (max-width: 1024px) {
-    width: 768px;
-  }
-  @media (max-width: 768px) {
-    width: 100%;
-  }
+  ${media.desktop`width: 768px;`}
+  ${media.tablet`width: 100%;`}
 `;
 
 const Button = styled.button`
